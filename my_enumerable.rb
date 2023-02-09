8e551024ca124e926d2ee1409164f06c1d7c4fa0
SekoVper <damehlivingstone1@gmail.com>
2023-02-09 14:48:44 +0000

    Solve ruby class problem

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -3,4 +3,15 @@ module MyEnumerable
     each { |n| return false unless block.call(n) }
     true
   end
+
+  def any?(&block)
+    each { |n| return true if block.call(n) }
+    false
+  end
+
+  def filter(&block)
+    new_array = []
+    each { |n| new_array << n if block.call(n) }
+    new_array
+  end
 end
